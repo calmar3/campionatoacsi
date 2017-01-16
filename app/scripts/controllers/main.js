900bd98abcdcd2ca2632b417d42472d24729b07e
calmar91 <m_calzetta@live.com>
2017-01-16 16:58:36 +0100

    customized timeline

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,21 +1,35 @@
-(function() {
+(function () {
   'use strict';
 
-  var MainCtrl = ['$scope', '$rootScope', '$compile', '$state', '$stateParams', function($scope, $rootScope, $compile, $state, $stateParams) {
+  var MainCtrl = ['$scope', '$rootScope', '$compile', '$state', '$stateParams','loadFactory', function ($scope, $rootScope, $compile, $state, $stateParams,loadFactory) {
 
     var ctrl = this;
 
     ctrl.changeState = changeStateFn;
 
-    function changeStateFn(){
+    ctrl.selectedTab = 0;
+
+    ctrl.selectTab = selectTabFn;
+
+    function selectTabFn(tab) {
+
+      ctrl.selectedTab = tab;
+
+    }
+
+    function changeStateFn() {
       $state.go('about');
     }
 
+    ctrl.calendario = loadFactory.getCalendario();
+    ctrl.calendario[0].avviso = "Garganta!";
+    
 
+    console.log(ctrl.calendario);
   }];
 
-  MainCtrl.$inject = ['$scope', '$rootScope', '$compile', '$state', '$stateParams'];
+  MainCtrl.$inject = ['$scope', '$rootScope', '$compile', '$state', '$stateParams','loadFactory'];
 
   angular.module('campionatoAcsi').controller('MainCtrl', MainCtrl);
 
-}());
+} ());
